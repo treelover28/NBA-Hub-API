1af88f0db300cb313eb86bef591362bea215b098
Khai Lai <akwardgamer08@gmail.com>
2020-04-01 23:50:57 -0600

    initial commit

--- api/app.py
+++ api/app.py
@@ -9,6 +9,8 @@ import json
 from flask_cors import CORS
 
 app = Eve()
+# automatically get port from Heroku
+port = int(os.environ.get("PORT", 5000))
 CORS(app)
 
 
@@ -64,4 +66,4 @@ def after_request(response):
     return response
 
 
-app.run(debug=True, host="0.0.0.0")
+app.run(debug=True, host="0.0.0.0", port=port)
